bd86e37028eb4bb2d31167e6e78ed499e66e38da
Pham Huu Hien <phhien203@gmail.com>
2023-04-06 20:58:28 +0700

    add userReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -9,8 +9,37 @@ export const UserContext = React.createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPE = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+function userReducer(state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPE.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled action type ${type} in userReducer`);
+  }
+}
+
 export function UserProvider({ children }) {
-  const [currentUser, setCurrentUser] = React.useState(null);
+  const [{ currentUser }, dispatch] = React.useReducer(
+    userReducer,
+    INITIAL_STATE
+  );
+
+  function setCurrentUser(user) {
+    dispatch({ type: USER_ACTION_TYPE.SET_CURRENT_USER, payload: user });
+  }
 
   React.useEffect(() => {
     const unSub = onAuthStateChangeListener((user) => {
