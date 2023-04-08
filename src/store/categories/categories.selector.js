f189c2632baeb8fd61235ea35aa11329fea7ce8c
Pham Huu Hien <phhien203@gmail.com>
2023-04-08 22:33:50 +0700

    memoize selector with reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,21 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categoriesList.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategories = (state) => state.categories;
+
+export const selectCategoriesList = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.categoriesList;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategoriesList],
+  (categoriesList) => {
+    return categoriesList.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,22 @@ import { applyMiddleware, compose, createStore } from "redux";
 import { rootReducers } from "./root-reducer";
 import logger from "redux-logger";
 
+const myLogger = function (store) {
+  return function (next) {
+    return function (action) {
+      if (!action.type) {
+        return next(action);
+      }
+
+      console.log(`type:`, action.type);
+      console.log(`payload:`, action.payload);
+      console.log(`current state:`, store.getState());
+
+      next(action);
+    };
+  };
+};
+
 const middleWares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
