1ce79e66d75f81a9a89d6946224b95e8aaafa6cc
Pham Huu Hien <phhien203@gmail.com>
2023-04-13 20:36:54 +0700

    add redux thunk

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -2,6 +2,8 @@ import { CATEGORIES_ACTION_TYPE } from "./categories.types";
 
 export const CATEGORIES_INITIAL_STATE = {
   categoriesList: [],
+  isLoading: false,
+  error: null,
 };
 
 export function categoriesReducer(
@@ -14,6 +16,23 @@ export function categoriesReducer(
     case CATEGORIES_ACTION_TYPE.SET_CATEGORIES_LIST:
       return {
         ...state,
+        categoriesList: payload,
+      };
+    case CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: payload,
+      };
+    case CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
         categoriesList: payload,
       };
     default:
--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,12 @@ import logger from "redux-logger";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducers } from "./root-reducer";
+import thunk from "redux-thunk";
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
     window &&
