6c92ba26bdbb011aa37e5f498f5f7392ab50fabd
Pham Huu Hien <phhien203@gmail.com>
2023-04-22 20:36:03 +0700

    add redux toolkit

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "@reduxjs/toolkit";
 import { cartReducer } from "./cart/cart.reducer";
 import { categoriesReducer } from "./categories/categories.reducer";
 import { userReducer } from "./user/user.reducer";
