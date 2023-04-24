be0e3169ba80dbd8ea619b893bb7e6ee0634b17f
Pham Huu Hien <phhien203@gmail.com>
2023-04-24 19:27:18 +0700

    add netlify function to handle payment

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -24,11 +24,37 @@ export default function PaymentForm() {
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const clientSecret = response.paymentIntent.client_secret;
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Hien Pham",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
   }
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment</h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASS.inverted}>Pay now</Button>
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,36 +6,41 @@ import {
   selectCartTotal,
 } from "../../store/cart/cart.selector";
 import "./checkout.styles.scss";
+import PaymentForm from "../../components/payment-form/payment-form";
 
 export default function Checkout() {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
 
   return (
-    <div className="checkout-container">
-      <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
+    <>
+      <div className="checkout-container">
+        <div className="checkout-header">
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className="header-block">
+            <span>Description</span>
+          </div>
+          <div className="header-block">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block">
+            <span>Price</span>
+          </div>
+          <div className="header-block">
+            <span>Remove</span>
+          </div>
         </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Price</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
+
+        {cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+
+        <div className="total">TOTAL: ${cartTotal}</div>
       </div>
 
-      {cartItems.map((cartItem) => (
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-
-      <div className="total">TOTAL: ${cartTotal}</div>
-    </div>
+      <PaymentForm />
+    </>
   );
 }
