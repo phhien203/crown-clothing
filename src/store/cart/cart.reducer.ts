f763cac989d3ca3d97c75102a75d1a47ed1281c9
Pham Huu Hien <phhien203@gmail.com>
2023-05-25 21:05:40 +0700

    migrate user selectors to TS

--- src/store/cart/cart.reducer.ts
+++ src/store/cart/cart.reducer.ts
@@ -3,8 +3,8 @@ import { setCartItems, setIsCartOpen } from "./cart.action";
 import { CartItem } from "./cart.types";
 
 export type CartState = {
-  cartItems: CartItem[];
-  isCartOpen: boolean;
+  readonly cartItems: CartItem[];
+  readonly isCartOpen: boolean;
 };
 
 export const CART_INITIAL_STATE: CartState = {
